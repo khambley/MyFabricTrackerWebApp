49fdca82fd8a87d9e831005c3367770d425bea58
Katherine Hambley <superdesigngirl@mac.com>
2020-07-09 13:12:19 -0500

    Added DbContext class and connection string

---
+++ MyFabricTrackerWebApp/Models/FabricTrackerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFabricTrackerWebApp.Models
+{
+	public class FabricTrackerDbContext : DbContext
+	{
+		public FabricTrackerDbContext(DbContextOptions<FabricTrackerDbContext> options) 
+			: base(options) { }
+
+		public DbSet<Fabric> Fabrics { get; set; }
+		public DbSet<MainCategory> MainCategories { get; set; }
+		public DbSet<SubCategory> SubCategories { get; set; }
+		public DbSet<Transaction> Transactions { get; set; }
+	}
+}
